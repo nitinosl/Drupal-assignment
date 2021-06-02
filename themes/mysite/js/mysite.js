e5ad974600b5ea15a4495d67354c9c94714708a4
nitin <Nitin.k@opensenselabs.com>
2021-06-02 10:29:45 +0530

    commit

--- themes/mysite/js/mysite.js
+++ themes/mysite/js/mysite.js
@@ -20,15 +20,10 @@ $(window).on('scroll', function() {
 
     function myFunction() {
         var x = document.getElementById("mynavdiv");
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
+        if (x.style.display === "block") {
           x.style.display = "none";
+        } else {
+          x.style.display = "block";
         }
       }
-
-      $(document).ready(function(){
-        $("#hamburger").click(function(){
-          $("#mynavdiv").toggle();
-        });
-      });
+      
